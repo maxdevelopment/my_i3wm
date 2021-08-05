798f46a7e45cdfe1501709054a095e2db6dbc71a
MaxN <piston-rod@tut.by>
2021-08-05 22:05:48 +0300

    config update

--- dual_monitors.sh
+++ dual_monitors.sh
@@ -6,10 +6,11 @@
 # Set up screens and set background
 if [ `xrandr | grep -c ' connected '` -eq 2 ]; then # dual-monitor
 	if [ `xrandr | grep HDMI-1 | grep -c ' connected '` -eq 1 ]; then
-	xrandr --output LVDS-1 --auto --output HDMI-1 --auto --primary --right-of LVDS-1
+	xrandr --output eDP-1 --auto --output HDMI-1 --auto --primary --left-of eDP-1
 	fi
 else
    xrandr --output eDP-1 --auto --primary
+   xrandr --output HDMI-1 --off
 fi
 feh --bg-scale ~/.i3/img/screen4.jpg
 cd ~/.i3/conky && ./startConky
--- polybar_launch.sh
+++ polybar_launch.sh
@@ -8,5 +8,8 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 env MONITOR=eDP-1 polybar --config=$HOME/.i3/polybar_config my_bar &
-#env MONITOR=HDMI-1 polybar --config=$HOME/.i3/polybar_config my_bar &
-
+if [ `xrandr | grep -c ' connected '` -eq 2 ]; then # dual-monitor
+	if [ `xrandr | grep HDMI-1 | grep -c ' connected '` -eq 1 ]; then
+		env MONITOR=HDMI-1 polybar --config=$HOME/.i3/polybar_config my_bar &
+	fi
+fi
