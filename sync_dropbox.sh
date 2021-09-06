7ae47c7f69bb4d7b77e948c88167b8db86a01bf5
MaxN <piston-rod@tut.by>
2021-09-06 15:49:30 +0300

    dunst icon

--- sync_dropbox.sh
+++ sync_dropbox.sh
@@ -4,4 +4,4 @@ ORG_DIR=/home/raw/MyDropbox
 REMOTE=MyDropbox
 
 rclone sync $ORG_DIR $REMOTE:
-notify-send 'Dropbox' 'MyDropbox folder has been synced'
+notify-send -u normal -i '/usr/share/icons/hicolor/24x24/apps/dropbox.png' 'Dropbox' 'MyDropbox folder has been synced'
